0dc655687bfcab7a0d690b17b66311cb22fb4c24
matheusilva2023 <matheus.silva.1083821@sga.pucminas.br>
2023-10-16 21:30:11 -0300

    Adicionado banco de dados a aplicação e incluido tabela Usuarios.

---
+++ medcenter-backend/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace medcenter_backend.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o CPF!")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a data de nascimento!")]
+        public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o email!")]
+        public string Email { get; set; }
+    }
+}
