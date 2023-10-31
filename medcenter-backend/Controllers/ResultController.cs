8bf015a9480972143068c4b75b5873e1bf8a70e5
Emile Ramos <emilemramos@gmail.com>
2023-10-30 22:09:54 -0300

    Exchange Exams for InfoExm

---
+++ medcenter-backend/Controllers/ResultController.cs
@@ -0,0 +1,30 @@
+﻿using medcenter_backend.Models.Pedidos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace medcenter_backend.Controllers
+{
+    public class ResultController : Controller
+    {
+        private static List<Result> _result = null;
+
+        public ResultController()
+        {
+            _result = new List<Result>();
+            Result result1 = new Result { 
+            PedidosAbertos = Guid.NewGuid().ToString() };
+
+            _result.Add(result1);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Listar()
+        {
+            return View(_result);
+        }
+
+    }
+}
