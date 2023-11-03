bb4142aa15d7bf70c471ca8f56972b9b630209f1
Guilherme Batista Brito <guizeravlogs2355@gmail.com>
2023-11-03 16:49:06 -0300

    Alterações feitas na pagian de result

--- medcenter-backend/Controllers/ResultController.cs
+++ medcenter-backend/Controllers/ResultController.cs
@@ -14,6 +14,20 @@ namespace medcenter_backend.Controllers
             PedidosAbertos = Guid.NewGuid().ToString() };
 
             _result.Add(result1);
+
+            _result = new List<Result>();
+            Result result2 = new Result
+            {
+                ProcedimentosAgendados = Guid.NewGuid().ToString()
+            };
+            _result.Add(result2);
+
+            _result = new List<Result>();
+            Result result3 = new Result
+            {
+                ProcedimentosRealizados = Guid.NewGuid().ToString()
+            };
+            _result.Add(result3);
         }
 
         public IActionResult Index()
