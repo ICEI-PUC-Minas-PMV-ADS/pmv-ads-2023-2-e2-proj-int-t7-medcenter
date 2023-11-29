45d00e4535f83fe435841bb86087821593faff59
Emile Ramos <emilemramos@gmail.com>
2023-11-28 23:13:50 -0300

    Info-Exms MVC

---
+++ medcenter-backend/Models/InfoExm.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace medcenter_backend.Models
+{
+    [Table("InfoExms")]
+    public class InfoExm
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        [Display(Name = "Descrição")]
+        public string Description { get; set; }
+        [Required]
+        public string Detalhes { get; set; }
+        [Required]
+        public string Preparo { get; set; }
+    }
+}
