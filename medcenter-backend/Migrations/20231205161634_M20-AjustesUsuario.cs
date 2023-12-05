86aabe9e2d39b23c0791ba48e4722954a13d9517
matheusilva2023 <matheus.silva.1083821@sga.pucminas.br>
2023-12-05 16:44:15 -0300

    incluindo metodo de esqueci minha senha.

---
+++ medcenter-backend/Migrations/20231205161634_M20-AjustesUsuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace medcenter_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class M20AjustesUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiracaoTokenRedefinicaoSenha",
+                table: "Usuarios",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiracaoTokenRedefinicaoSenha",
+                table: "Usuarios");
+        }
+    }
+}
